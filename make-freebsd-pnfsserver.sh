6e15d76592855db8e821e5882f79f17fda781483
Jianhong Yin <jiyin@redhat.com>
2024-10-17 08:38:23 +0800

    optimize code. and remove vm-cpto -v option seems it can break the
    vm-exec without password, maybe it's a kiss-vm bug
    
    Signed-off-by: Jianhong Yin <yin-jianhong@163.com>

--- make-freebsd-pnfsserver.sh
+++ make-freebsd-pnfsserver.sh
@@ -69,8 +69,9 @@ echo -e "\n{INFO} waiting VMs install finish ..."
 for dsserver in $vm_ds1 $vm_ds2; do
 	until port_available ${dsserver} 22; do sleep 2; done; sleep 2
 	echo -e "\n{INFO} setup ${dsserver}:"
-	vm cpto -v ${dsserver} freebsd-pnfs-ds.sh .
-	vm exec -v ${dsserver} sh freebsd-pnfs-ds.sh
+	cpfile=freebsd-pnfs-ds.sh; [[ -f "$cpfile" ]] || cpfile=/usr/bin/$cpfile
+	vm cpto    ${dsserver} $cpfile /usr/bin
+	vm exec -v ${dsserver} $cpfile
 	vm exec -v ${dsserver} -- showmount -e localhost
 done
 
@@ -79,16 +80,18 @@ echo -e "\n{INFO} setup ${vm_mds}:"
 ds1addr=$(vm ifaddr $vm_ds1)
 ds2addr=$(vm ifaddr $vm_ds2)
 until port_available ${vm_mds} 22; do sleep 2; done; sleep 2
-vm cpto -v ${vm_mds} freebsd-pnfs-mds.sh .
-vm exec -v ${vm_mds} sh freebsd-pnfs-mds.sh $ds1addr $ds2addr
+cpfile=freebsd-pnfs-mds.sh; [[ -f "$cpfile" ]] || cpfile=/usr/bin/$cpfile
+vm cpto    ${vm_mds} $cpfile /usr/bin
+vm exec -v ${vm_mds} $cpfile $ds1addr $ds2addr
 vm exec -v ${vm_mds} -- mount -t nfs
 vm exec -v ${vm_mds} -- showmount -e localhost
 
 #config freebsd pnfs client
 echo -e "\n{INFO} setup ${vm_fbclient}:"
 until port_available ${vm_fbclient} 22; do sleep 2; done; sleep 2
-vm cpto -v ${vm_fbclient} freebsd-pnfs-client.sh .
-vm exec -v ${vm_fbclient} sh freebsd-pnfs-client.sh
+cpfile=freebsd-pnfs-client.sh; [[ -f "$cpfile" ]] || cpfile=/usr/bin/$cpfile
+vm cpto    ${vm_fbclient} $cpfile /usr/bin
+vm exec -v ${vm_fbclient} ${cpfile}
 
 #mount test from freebsd client
 expdir0=/export0
